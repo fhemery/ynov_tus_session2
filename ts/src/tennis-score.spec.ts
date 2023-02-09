43dd212b76e381f040b1e161ab86ccce82d2a2d0
fhemery <f.hemery@hemit.fr>
2023-02-09 12:27:40 +0100

    [TS] Simple example on how to write unit tests

---
+++ ts/src/tennis-score.spec.ts
@@ -0,0 +1,37 @@
+import { TennisScore } from "./tennis-score";
+
+describe("TennisScore", function() {
+  let tennisScore: TennisScore;
+
+  beforeEach(() => {
+    tennisScore = new TennisScore();
+  })
+
+  it("should return Love All by default", function() {
+    expect(tennisScore.getScore()).toBe('0 - 0');
+  });
+
+  it("should return 15-0 if player1 scores", function() {
+    tennisScore.player1Scores();
+
+    expect(tennisScore.getScore()).toBe('15 - 0');
+  });
+
+  it("should return 30-0 if player1 scores twice", function() {
+    tennisScore.player1Scores().player1Scores();
+
+    expect(tennisScore.getScore()).toBe('30 - 0');
+  });
+
+  it("should return Adv to player 2 if player2 scores at 40 All", function() {
+    tennisScore.player1Scores().player1Scores().player1Scores().player2Scores().player2Scores().player2Scores().player2Scores();
+
+    expect(tennisScore.getScore()).toBe('40 - Adv.');
+  });
+
+  it("should return Adv to player 1 if player1 scores at 40 All", function() {
+    tennisScore.player1Scores().player1Scores().player1Scores().player2Scores().player2Scores().player2Scores().player1Scores();
+
+    expect(tennisScore.getScore()).toBe('Adv. - 40');
+  });
+});
