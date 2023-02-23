0eccaf217186023f171705f7340878b4cbc7fae7
fhemery <f.hemery@hemit.fr>
2023-02-23 11:31:44 +0100

    [TS]: trip service refactoring

--- ts/src/trip-service/trip/TripService.ts
+++ ts/src/trip-service/trip/TripService.ts
@@ -7,26 +7,33 @@ import TripDAO from "./TripDAO";
 export default class TripService {
 
     public getTripsByUser(user: User): Trip[] {
-        let tripList: Trip[] = [];
+        const loggedUser = this.getUser();
+
+        if (this.isFriendWith(user, loggedUser)) {
+            return this.getTrips(user);
+        }
+
+        return [];
+    }
+
+    private getUser() {
         const loggedUser: User | null = this.getLoggedUser();
-        let isFriend = false;
 
-        if (loggedUser != null) {
-            for (const friend of user.getFriends()) {
-                if (friend === loggedUser) {
-                    isFriend = true;
-                    break;
-                }
-            }
-
-            if (isFriend) {
-                tripList = this.getTrips(user);
-            }
-
-            return tripList;
-        } else {
+        if (loggedUser === null) {
             throw new UserNotLoggedInException();
         }
+        return loggedUser;
+    }
+
+    private isFriendWith(user: User, loggedUser: User) {
+        let isFriend = false;
+        for (const friend of user.getFriends()) {
+            if (friend === loggedUser) {
+                isFriend = true;
+                break;
+            }
+        }
+        return isFriend;
     }
 
     protected getTrips(user: User) {
